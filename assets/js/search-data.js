08c1da6580ba46eea14c21b226a3d5e0c457bad5
aryavahedi <39989822+aryavahedi@users.noreply.github.com>
2026-04-17 14:32:47 +0000

    Deploying to gh-pages from @ aryavahedi/OfArya@0575fab4e5b2b389b80898d77a8b57655fc880d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,12 +34,7 @@ ninja.data = [{
             window.location.href = "/OfArya/blog/2026/formatting-and-links/";
           
         },
-      },{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/OfArya/projects/1_project/";
-            },},{
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
